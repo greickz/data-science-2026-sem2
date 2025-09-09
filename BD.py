e9d54ef98564fd8ce73923c0b6fbc84960a29317
VINÍCIUS GREICK DA SILVA OLIVEIRA <47377942878@info.local>
2025-09-08 21:26:46 -0300

    aula 1

---
+++ BD.py
@@ -0,0 +1,40 @@
+# Simulação do banco de dados
+
+Carros = [
+
+    { # {} = conjunto de informações
+        'id': 1,
+        'marca': 'Fiat',
+        'modelo': 'Marea',
+        'ano': 1999
+    },
+
+    { 
+        'id': 2,
+        'marca': 'Ford',
+        'modelo': 'Maverik',
+        'ano': 1975
+    },
+
+    { 
+        'id': 3,
+        'marca': 'Chevrolet',
+        'modelo': 'Opala',
+        'ano': 1970
+    },
+
+    { 
+        'id': 4,
+        'marca': 'Mitsubishi',
+        'modelo': 'Lancer',
+        'ano': 1994
+    },
+
+    { 
+        'id': 5,
+        'marca': 'Honda',
+        'modelo': 'Civic',
+        'ano': 2002
+    }
+
+]
